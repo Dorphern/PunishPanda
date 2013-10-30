32a0939748440a64ea9decf2ba2611873a12e807
Dimitrios Katsaros <rth738@alumni.ku.dk>
2013-10-30 15:20:15 +0100

    Fixed CollisionController

--- Assets/Scripts/CollisionController.cs
+++ Assets/Scripts/CollisionController.cs
@@ -3,22 +3,40 @@ using System.Collections;
 
 public class CollisionController : MonoBehaviour {
 	
+	/* 
+	 * 
+	 * Collision Controller
+	 * 
+	 * Provides delegates for type specific collision and trigger handling
+	 * For an object to have a type, it must have the Collidable script attached to it
+	 * If the None(default) type is set, none of the type specific handlers is fired.
+	 * 
+	 * The default delegates DefaultOnHit, DefaultOnTriggerEnter, DefaultOnTriggerExit
+	 * DefaultOnTriggerStay are fired always.
+	 *
+	 */
+	
 	
 	// delegates
 	public event System.Action<ControllerColliderHit> OnPandaHit;
 	public event System.Action<ControllerColliderHit> OnWallHit;
 	
 	
+	//defaultDelegates
+	public event System.Action<ControllerColliderHit> DefaultOnHit;
+	public event System.Action<Collider> DefaultOnTriggerEnter;
+	public event System.Action<Collider> DefaultOnTriggerExit;
+	public event System.Action<Collider> DefaultOnTriggerStay;
 	
 	
-	void OnControllerColliderHit(ControllerColliderHit hit) {
+	void OnControllerColliderHit(ControllerColliderHit hit) 
+	{
 		
 		var collidable = hit.collider.GetComponent<Collidable>();
 		
 		//check if the object we collided with has a collidable
 		if(collidable!=null)
-		{
-			
+		{	
 			//type collision cases
 			if(collidable.type == CollidableTypes.Panda && OnPandaHit!=null) 
 			{
@@ -31,6 +49,38 @@ public class CollisionController : MonoBehaviour {
 			}
 			
 			
-		}		
+		}
+		//Default delegate
+		if(DefaultOnHit!=null)
+		{
+			DefaultOnHit(hit);	
+		}
+		
 	}
+	
+	void OnTriggerEnter(Collider c) 
+	{
+        
+		
+		//Default delegate
+		if(DefaultOnTriggerEnter!=null)
+			DefaultOnTriggerEnter(c);
+		
+    }
+	
+	void OnTriggerExit(Collider other) 
+	{
+        
+		//Default delegate
+		if(DefaultOnTriggerEnter!=null)
+			DefaultOnTriggerExit(c);
+    }
+	
+	void OnTriggerStay(Collider other) 
+	{
+        
+		//Default delegate
+		if(DefaultOnTriggerEnter!=null)
+			DefaultOnTriggerStay(c);
+    }
 }
