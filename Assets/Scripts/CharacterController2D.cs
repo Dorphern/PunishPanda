d815f51efec3558e90356284937f69d8dbdb600e
RubenKLopez <RLopez5392@gmail.com>
2013-10-30 15:00:29 +0100

    Restore character controller

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -10,12 +10,11 @@ public Movement movement;
  
 private CharacterController controller;
 	
-	//MY VARIABLES:
+	
 	public CollisionController _collisionController;
-	//PROTOTYPE PANDA STATES:
+	//PLACE-HOLDER PANDA STATES:
 	private bool walkingRight;
 	private bool walkingLeft;
-	public bool liftingState;
  
 	/***********************************************************************
 	 * Classes
@@ -27,37 +26,18 @@ private CharacterController controller;
 	     * Public (Inspector) variables
 	     *******************************/
 	 
-	    public bool  enabled = true; // Is the character controller enabled?
 	    public float gravity = 20;
 	    public float jumpHeight = 8;
 	    public float walkSpeed = 3;
-	    public bool  rotate = false; // Should the character rotate?
-	    public bool  rotateIn3D = false; // Should the character be rotated in 3D?
-	    public float rotationSmoothing = 10; // Rotation Smoothing speed (for "Rotate In 3D")
 	 
 	    /*******************************
 	     * NonSerialized variables
 	     *******************************/
-	 
-	    // The character's current horizontal direction
-	    [System.NonSerialized]
-	    public float horizontalDirection;
-	 
-	    // The character's current direction (on all planes)
-	    [System.NonSerialized]
-	    public Vector3 direction;
-	 
-	    // The character's current movement offset
+
+	    // The character's current movement offset (for Jumping / Falling)
 	    [System.NonSerialized]
 	    public Vector3 offset;
 	 
-	    /*******************************
-	     * Jumping
-	     *******************************/
-	 
-	    // Did the user press the jump input button while midair?
-	    [System.NonSerialized]
-	    public bool usedExtraJump = false;
 	}
  
 	/***********************************************************************
@@ -66,21 +46,19 @@ private CharacterController controller;
 	 
 	void  Awake (){
 	    controller = GetComponent<CharacterController>();
-		//Dont use spawnpoint  for now...
-	    //Spawn();
 		
+		//NOT USING A SPAWN POINT FOR NOW...
+	    //Spawn();
 		
 		//START WALKING RIGHT INITIALLY..
 		walkingRight = true;
 		walkingLeft = false;
 		
-		//_collisionController.OnPandaHit += changeDirection;
+		//CHANGE DIRECTION when hits pandas or walls
+		_collisionController.OnPandaHit += changeDirection;
+		_collisionController.OnWallHit += changeDirection;
+		
 	}
-
-    void Stop()
-    {
-        //_collisionController.OnPandaHit -= changeDirection;
-    }
 	 
 	void  FixedUpdate (){
 	    // Make sure the character stays in the 2D plane
@@ -88,10 +66,7 @@ private CharacterController controller;
 	}
 	 
 	void  Update (){
-	    // GetAxisRaw("Horizontal") will return either -1 or 1 while the left or right input is depressed
-	    // Otherwise, GetAxisRaw("Horizontal") returns 0 for no input
-	    movement.horizontalDirection = (movement.enabled) ? Input.GetAxisRaw("Horizontal") : 0;
-	    if (movement.horizontalDirection != 0) movement.direction = new Vector3(movement.horizontalDirection, 0, 0);
+
 	    MoveCharacter();
 	}
 	 
@@ -105,19 +80,16 @@ private CharacterController controller;
 	 
 	// Move the character using Unity's CharacterController.Move function
 	void  MoveCharacter (){
-	    //ApplyMovement();
-	    //ApplyGravity();
-	    // Move the character (with deltaTime to ensure frame rate independence)
-		
-		
-	    //controller.Move(movement.offset * Time.deltaTime);
-		
+	    HandleJump();
+	    ApplyGravity();
+			
 		if(walkingRight == true  && walkingLeft == false)
 			controller.Move(Vector3.right * movement.walkSpeed * Time.deltaTime);
 		
 		if(walkingLeft == true && walkingRight == false)
 			controller.Move(Vector3.left * movement.walkSpeed * Time.deltaTime);
-		
+		 
+		//USING "OFFSET" FOR APPLYING GRAVITY and/or JUMPING
 		controller.Move(movement.offset * Time.deltaTime);
 	}
 	
@@ -131,50 +103,31 @@ private CharacterController controller;
 		}
 		
 	}
-	 
-	void  ApplyMovement (){
+	
+	void  HandleJump (){
 	    switch (controller.isGrounded) {
 	        // The character is on the ground
 	        case true:
-	            movement.usedExtraJump = false;
-	            if (movement.enabled) {
-	                movement.offset = new Vector3(Input.GetAxis("Horizontal"), 0, 0) * movement.walkSpeed;
-	                if (Input.GetButtonDown("Jump")) ApplyJump();
-	            } else {
-	                movement.offset = Vector3.zero;
-	            }
+	                if (Input.GetButtonDown("Jump")){ 
+						ApplyJump();
+					}else{
+						movement.offset = Vector3.zero;
+					}
 	        break;
 	 
 	        // The character is midair
 	        case false:
-	            if (movement.enabled) {
-	                movement.offset.x = Input.GetAxis("Horizontal") * movement.walkSpeed;
-	                // Apply an extra jump if the jump input button is pressed a second time
-	                // The final jump height will be greatest at the apex of the first jump
-	                if (Input.GetButtonDown("Jump") && ! movement.usedExtraJump) {
-	                    movement.usedExtraJump = true;
-	                    ApplyJump();
-	                }
-	            }
+	            //DO NOTHING (i.e. dont keep applying jump)
 	        break;
 	    }
 	 
-	    //if (movement.rotate) ApplyRotation();
 	}
-	 
+	
+	
 	void  ApplyJump (){
 	    movement.offset.y = movement.jumpHeight;
 	}
-	 
-	void  ApplyRotation (ControllerColliderHit c){
-	    if (movement.direction.x != 0) {
-	        if (movement.rotateIn3D) {
-	            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement.direction), Time.deltaTime * movement.rotationSmoothing);
-	        } else {
-	            transform.rotation = Quaternion.LookRotation(movement.direction);
-	        }
-	    }
-	}
+	  
 	 
 	void  ApplyGravity (){
 	    movement.offset.y -= movement.gravity * Time.deltaTime;
