5e94411de562746d971e459753cf9b0539f9b45e
Dimitrios Katsaros <rth738@alumni.ku.dk>
2013-10-30 16:02:23 +0100

    Created PandaAI

---
+++ Assets/Scripts/PandaAI.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class PandaAI : MonoBehaviour {
+
+	PandaStateManager _pandaStateManager;
+	CollisionController _collisionController;
+	
+	
+	
+	# region Private Methods
+	// Use this for initialization
+	void Start () {
+		_pandaStateManager = GetComponent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	# endregion
+}
