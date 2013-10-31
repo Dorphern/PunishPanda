66e5fb5e80a629fe5296e601e78658d5bdb61562
Lund <lunduz@gmail.com>
2013-10-31 16:39:38 +0100

    Added Swipe

--- Assets/Scripts/FingerBlocking.cs
+++ Assets/Scripts/FingerBlocking.cs
@@ -6,10 +6,24 @@ public class FingerBlocking : MonoBehaviour {
 
 	//cameraOffset determines where to place blockade on Z-axis
 	private float cameraOffset;
+    private Transform boxTransform;
+    public float minVelocity;
+    private Vector3 lastPosition;
+    public Vector3 currentVel;
 	
 	void Start () {
 		collider.enabled = false;
+        boxTransform = gameObject.transform;
+        lastPosition = Vector3.zero;
 	}
+
+	
+	void Update () {
+
+        lastPosition = boxTransform.position;
+        StartCoroutine(DeltaPosition(lastPosition));
+	} 
+	
 	
 	public void ActivateBlockade (Vector3 mousePos)
 	{
@@ -18,8 +32,10 @@ public class FingerBlocking : MonoBehaviour {
 			
 		mousePos.z = cameraOffset;
 			
+ 
         Vector3 objectPos = Camera.main.ScreenToWorldPoint(mousePos);
-			
+
+        collider.isTrigger = false;	
 		collider.enabled = true;
        	transform.position = objectPos;
 		
@@ -29,5 +45,17 @@ public class FingerBlocking : MonoBehaviour {
 	{
 		collider.enabled = false;
 	}
-	
+
+   IEnumerator DeltaPosition(Vector3 lastPos)
+    {
+       
+        yield return new WaitForEndOfFrame();
+        currentVel = (lastPos - boxTransform.position) / Time.deltaTime;
+        Debug.Log(currentVel.magnitude);
+
+        if (currentVel.magnitude > minVelocity)
+        {
+            collider.isTrigger = true;
+        }
+    }
 }
