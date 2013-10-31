296eb33a7a08434cf0856f4122252c157fc31a2e
cristi16 <patrascu.cristinel@gmail.com>
2013-10-31 09:54:32 +0100

    finger blocking input handling

--- Assets/Scripts/FingerBlocking.cs
+++ Assets/Scripts/FingerBlocking.cs
@@ -42,7 +42,7 @@ public class FingerBlocking : MonoBehaviour {
 	} */
 	
 	
-	void ActivateBlockade (Vector3 mousePos)
+	public void ActivateBlockade (Vector3 mousePos)
 	{
 		cameraOffset = Camera.main.transform.position.z;
 		cameraOffset = Mathf.Abs(cameraOffset);
@@ -58,7 +58,7 @@ public class FingerBlocking : MonoBehaviour {
 		
 	}
 	
-	void DeactivateBlockade()
+	public void DeactivateBlockade()
 	{
 		collider.enabled = false;
 	}
--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -7,10 +7,11 @@ public class InputHandler : MonoBehaviour {
 	private Ray ray;
 	private RaycastHit hitInfo;
 	private PandaStateManager pandaStateManager;
+	private FingerBlocking fingerBlockade;
 	
 	void Start () 
 	{
-		
+		fingerBlockade = GameObject.FindGameObjectWithTag("FingerBlockade").GetComponent<FingerBlocking>();
 	}
 	
 	void Update () 
@@ -27,7 +28,16 @@ public class InputHandler : MonoBehaviour {
 					pandaStateManager = hitInfo.collider.GetComponent<PandaStateManager>();
 					pandaStateManager.ChangeState(PandaState.HoldingOntoFinger);	
 				}
-			}
+				
+			}		
+		}
+		
+		if(Input.GetMouseButton(0))
+		{
+			if(pandaStateManager == null)
+			{
+				fingerBlockade.ActivateBlockade(Input.mousePosition);
+			}	
 		}
 		
 		if(Input.GetMouseButtonUp(0))
@@ -37,6 +47,8 @@ public class InputHandler : MonoBehaviour {
 				pandaStateManager.ChangeState(PandaState.Walking);
 				pandaStateManager = null;
 			}
+			
+			fingerBlockade.DeactivateBlockade();
 		}
 	}
 }
