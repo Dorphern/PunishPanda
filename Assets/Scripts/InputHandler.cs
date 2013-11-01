fa74eac15831aa3e5ea8d486d2a430a1227de014
cristi16 <patrascu.cristinel@gmail.com>
2013-11-01 13:31:20 +0100

    refactored input handler

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -55,45 +55,86 @@ public class InputHandler : MonoBehaviour {
 			// Touch began
 			if(touch.phase == TouchPhase.Began)
 			{
-				DetectCollidable(touch.position, touch.fingerId);
+				PerformTouchBegan(touch.position, touch.fingerId);
 			}
 			
-			if(selectedPandas.ContainsKey(touch.fingerId))
-			{
-				selectedPandas.TryGetValue(touch.fingerId, out tempPanda);
-				// if our fingerID corresponds with a panda we updated the position on PandaAI
-				tempPanda.touchPosition = touch.position;
-			}
-			else if(selectedBlockades.ContainsKey(touch.fingerId))
-			{
-				selectedBlockades.TryGetValue(touch.fingerId, out tempBlockade);
-				tempBlockade.ActivateBlockade(touch.position);	
-			}
+			PerformTouchUpdate(touch);
 			
 			// Touch ended
 			if(touch.phase == TouchPhase.Ended)
 			{
-				if(selectedPandas.ContainsKey(touch.fingerId))
-				{
-					selectedPandas.TryGetValue(touch.fingerId, out tempPanda);
-					tempPanda.PandaReleased();
-					selectedPandas.Remove(touch.fingerId);
-				}
-				else if(selectedBlockades.ContainsKey(touch.fingerId))
-				{
-					selectedBlockades.TryGetValue(touch.fingerId, out tempBlockade);
-					tempBlockade.DeactivateBlockade();
-					blockades.Add(tempBlockade);
-					selectedBlockades.Remove(touch.fingerId);
-				}
-				else if(selectedHotSpots.ContainsKey(touch.fingerId))
-				{
-					selectedHotSpots.TryGetValue(touch.fingerId, out tempHotSpot);
-					tempHotSpot.DeactivateHotspot();
-					selectedHotSpots.Remove(touch.fingerId);
-				}
+				PerformTouchEnded(touch.fingerId);
 			}
 		}			
+	}
+	
+	void PerformTouchBegan(Vector3 position, int fingerID)
+	{
+		ray = Camera.main.ScreenPointToRay(position);
+		if(Physics.SphereCast(ray, fingerRadius, out hitInfo))
+		{
+			Collidable collidable = hitInfo.collider.GetComponent<Collidable>();
+		
+			if(collidable != null)
+			{
+				if(collidable.type == CollidableTypes.Panda)
+				{
+					tempPanda = hitInfo.collider.GetComponent<PandaAI>();
+					tempPanda.PandaPressed();
+					tempPanda.touchPosition = position;
+					selectedPandas.Add(fingerID, tempPanda);
+					return;
+				}
+				else if(collidable.type == CollidableTypes.Hotspot)
+				{
+					tempHotSpot = hitInfo.transform.parent.GetComponent<Hotspot>();
+					tempHotSpot.ActivateHotspot();
+					selectedHotSpots.Add(fingerID, tempHotSpot);
+					return;
+				}
+			}			
+		}
+		// we activate a blockade 
+		selectedBlockades.Add(fingerID,  blockades[0]);
+		blockades.RemoveAt(0);
+	}
+	
+	void PerformTouchUpdate(Touch touch)
+	{
+		if(selectedPandas.ContainsKey(touch.fingerId))
+		{
+			selectedPandas.TryGetValue(touch.fingerId, out tempPanda);
+			// if our fingerID corresponds with a panda we updated the position on PandaAI
+			tempPanda.touchPosition = touch.position;
+		}
+		else if(selectedBlockades.ContainsKey(touch.fingerId))
+		{
+			selectedBlockades.TryGetValue(touch.fingerId, out tempBlockade);
+			tempBlockade.ActivateBlockade(touch.position);	
+		}
+	}
+	
+	void PerformTouchEnded(int fingerId)
+	{	
+		if(selectedPandas.ContainsKey(fingerId))
+		{
+			selectedPandas.TryGetValue(fingerId, out tempPanda);
+			tempPanda.PandaReleased();
+			selectedPandas.Remove(fingerId);
+		}
+		else if(selectedBlockades.ContainsKey(fingerId))
+		{
+			selectedBlockades.TryGetValue(fingerId, out tempBlockade);
+			tempBlockade.DeactivateBlockade();
+			blockades.Add(tempBlockade);
+			selectedBlockades.Remove(fingerId);
+		}
+		else if(selectedHotSpots.ContainsKey(fingerId))
+		{
+			selectedHotSpots.TryGetValue(fingerId, out tempHotSpot);
+			tempHotSpot.DeactivateHotspot();
+			selectedHotSpots.Remove(fingerId);
+		}	
 	}
 	
 	void MouseUpdate()
@@ -124,13 +165,13 @@ public class InputHandler : MonoBehaviour {
 		
 		if(Input.GetMouseButton(0))
 		{
-			if(tempPanda == null)
-			{
-				blockades[0].ActivateBlockade(Input.mousePosition);
-			}
-			else
+			if(tempPanda != null)
 			{
 				tempPanda.touchPosition = Input.mousePosition;	
+			}
+			else if(tempHotSpot == null)
+			{
+				blockades[0].ActivateBlockade(Input.mousePosition);
 			}
 		}
 		
@@ -150,38 +191,6 @@ public class InputHandler : MonoBehaviour {
 			{
 				blockades[0].DeactivateBlockade();
 			}
-		}
-		
+		}	
 	}
-	
-	void DetectCollidable(Vector3 position, int fingerID)
-	{
-		ray = Camera.main.ScreenPointToRay(position);
-		if(Physics.SphereCast(ray, fingerRadius, out hitInfo))
-		{
-			Collidable collidable = hitInfo.collider.GetComponent<Collidable>();
-		
-			if(collidable != null)
-			{
-				if(collidable.type == CollidableTypes.Panda)
-				{
-					tempPanda = hitInfo.collider.GetComponent<PandaAI>();
-					tempPanda.PandaPressed();
-					tempPanda.touchPosition = position;
-					selectedPandas.Add(fingerID, tempPanda);
-					return;
-				}
-				else if(collidable.type == CollidableTypes.Hotspot)
-				{
-					tempHotSpot = hitInfo.transform.parent.GetComponent<Hotspot>();
-					tempHotSpot.ActivateHotspot();
-					selectedHotSpots.Add(fingerID, tempHotSpot);
-					return;
-				}
-			}			
-		}
-		// we activate a blockade 
-		selectedBlockades.Add(fingerID,  blockades[0]);
-		blockades.RemoveAt(0);
-	}	
 }
