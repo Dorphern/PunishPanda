c6b08925a6aa5d54a1d0f825bcc56fef771b6261
Lund <lunduz@gmail.com>
2013-11-05 09:14:13 +0100

    PointList

---
+++ Assets/Scripts/InputPointsList.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputPointsList : MonoBehaviour {
+
+    public Vector2 inputPoint;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void listOfInputPOints(Vector2 iP)
+    {
+        inputPoint = iP;
+    }
+
+
+}
