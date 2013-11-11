87e2793d4102ec41151ce37df23fbaad92d501ad
Kristoffer Dorph <kristofferdo@gmail.com>
2013-11-11 15:48:52 +0100

    Added dirty trap state

--- Assets/Scripts/PandaAI.cs
+++ Assets/Scripts/PandaAI.cs
@@ -109,13 +109,14 @@ public class PandaAI : MonoBehaviour {
      **/
     public bool AttemptDeathTrapKill (TrapBase trap, bool isPerfect)
     {
-        Debug.Log("Hit death object: " + trap.GetTrapType());
+        float direction = Vector3.Angle(trap.transform.position, transform.position);
         pandaStateManager.ChangeState(PandaState.Died);
+        Debug.Log("Panda died from " + trap.GetTrapType() + "; direction: " + direction);
         return true;
     }
 	#endregion
 	
-	# region Private Methods
+    # region Private Methods
 	// Use this for initialization
 	void Start()
 	{
@@ -188,7 +189,7 @@ public class PandaAI : MonoBehaviour {
 		// make sure some time has passed since the last collision
 		if(Time.time - timeSinceLastCollisionWithPanda < pandaCollisionDelay)
 			return;
-	
+
 		timeSinceLastCollisionWithPanda = Time.time;
 		
 		
@@ -229,6 +230,10 @@ public class PandaAI : MonoBehaviour {
 			// if we hit a panda that is holding on to the finger we want this panda to change direction
 			else if(otherPandaSM.GetState() ==  PandaState.HoldingOntoFinger)
 				pandaStateManager.SwapDirection(pandaStateManager.GetDirection());
+
+            else if (otherPandaSM.GetState() == PandaState.Died)
+                pandaStateManager.SwapDirection(pandaStateManager.GetDirection());
+
 		}
 	}
 	
