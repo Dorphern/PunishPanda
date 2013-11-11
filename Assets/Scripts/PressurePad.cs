487dd6522fec1f96d7dcb341dcc61267dc7ba224
cristi16 <patrascu.cristinel@gmail.com>
2013-11-11 12:39:40 +0100

    Basis for pressurePad

---
+++ Assets/Scripts/PressurePad.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePad : MonoBehaviour {
+	
+	public GameObject trap;
+	private int pandaCount = 0;
+	
+	void Start() 
+	{
+	
+	}
+	
+	void Update() 
+	{
+		if(pandaCount > 0)
+		{
+			// trap.activateTrap();
+			trap.animation.Play();
+		}
+		else
+		{
+			// trap.DeactivateTrap();	
+		}
+	}
+	
+	void OnTriggerEnter(Collider collider)
+	{
+		if(collider.GetComponent<Collidable>().type == CollidableTypes.Panda)
+		{
+			pandaCount++;
+		}
+	}
+	
+	void OnTriggerExit(Collider collider)
+	{
+		if(collider.GetComponent<Collidable>().type == CollidableTypes.Panda)
+		{
+			pandaCount--;
+		}
+	}
+}
