4dd295d48cc94d13a4f415088bbbc7a507844341
cristi16 <patrascu.cristinel@gmail.com>
2013-11-11 15:08:37 +0100

    gameplay elements (button, level, pressure pad, trapdoor)

---
+++ Assets/Scripts/SideWallButton.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideWallButton : MonoBehaviour {
+
+	public TrapBase trap;
+	private AnimationState trapClip;
+	public float activationTimeLength = 3f;
+	
+	void Start () 
+	{
+		trapClip = trap.animation["DoorTrap"];
+	}
+	
+	void Update () 
+	{
+		
+	}
+	
+	void OnTriggerEnter(Collider collider)
+	{
+		if(collider.GetComponent<Collidable>().type == CollidableTypes.Panda)
+		{
+			if(trap.isActive() == false)
+			{
+				trap.ActivateTrap();
+				trapClip.speed = 1f;
+				trapClip.time = 0f;
+				trap.animation.Play();
+				StartCoroutine(DeactivateTrap());
+			}
+		}
+	}
+	
+	IEnumerator DeactivateTrap()
+	{
+		yield return new WaitForSeconds(activationTimeLength);
+		
+		trap.DeactivateTrap();
+		trapClip.speed = -1f;
+		trapClip.time = trapClip.length;
+		trap.animation.Play();
+	}
+}
---
+++ Assets/Scripts/Traps/DoorTrap.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTrap : TrapBase {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	protected override bool PandaAttemptKill (PandaAI pandaAI, bool isPerfect)
+	{
+		return true;	
+	}
+}
