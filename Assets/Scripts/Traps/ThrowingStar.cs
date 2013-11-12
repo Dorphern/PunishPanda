625e53d1881a135ae3f7647923553bc4a78ef16a
cristi16 <patrascu.cristinel@gmail.com>
2013-11-12 15:27:08 +0100

    implemented throwing stars

---
+++ Assets/Scripts/Traps/ThrowingStar.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ThrowingStar : MonoBehaviour 
+{
+	[System.NonSerialized] public StarSpawner starSpawner;
+	private bool isActive = true;
+	
+	public void ShootStar(Vector3 direction, float force, float torque) 
+	{
+		isActive = true;
+		rigidbody.useGravity = false;
+		rigidbody.isKinematic = false;
+		rigidbody.AddForce(direction * force, ForceMode.Impulse);
+		rigidbody.AddTorque( - Vector3.forward * torque, ForceMode.Impulse);
+	}
+	
+	protected void OnTriggerEnter (Collider collider)
+    {
+		if(collider.tag == "FingerBlockade" && isActive == true)
+		{
+			this.isActive = false;
+			rigidbody.velocity = Vector3.zero;
+			rigidbody.useGravity = true;
+		}
+		
+        Collidable collidable = collider.GetComponent<Collidable>();
+		if (collidable == null) return;
+		
+        if (collidable.type == CollidableTypes.Panda && isActive == true)
+        {
+            starSpawner.TryPandaKill(collider.GetComponent<PandaAI>());
+        }
+		else if(collidable.type == CollidableTypes.Wall)
+		{
+			this.isActive = false;
+			rigidbody.isKinematic = true;
+		}
+		else if(collidable.type == CollidableTypes.ThrowingStar)
+		{
+			if(collider.GetComponent<ThrowingStar>().isActive == true)
+			{
+				// pool objects instead of destroying them
+				// remove object in a coroutine after a short amount
+				//starSpawner.ReuseThrowingStar(collider.GetComponent<ThrowingStar>());
+				this.enabled = false;
+				renderer.enabled = false;
+			}
+		}
+		else if(collidable.type == CollidableTypes.Floor)
+		{
+			this.isActive = false;
+			rigidbody.isKinematic = true;
+			GameObject emptyObject = new GameObject();
+			emptyObject.transform.parent = collider.transform;
+			this.transform.parent = emptyObject.transform;
+		}
+    }
+}
