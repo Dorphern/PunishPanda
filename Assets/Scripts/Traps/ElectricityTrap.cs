3da4cad4bff045b2c107695400b04a479eb5836a
Kristoffer Dorph <kristofferdo@gmail.com>
2013-11-12 15:25:26 +0100

    Base electricity

--- Assets/Scripts/Traps/ElectricityTrap.cs
+++ Assets/Scripts/Traps/ElectricityTrap.cs
@@ -3,12 +3,29 @@ using System.Collections;
 
 public class ElectricityTrap : TrapBase {
 
+    [SerializeField] GameObject electricity;
+    [SerializeField] GameObject electricityBox0;
+    [SerializeField] GameObject electricityBox1;
+
     override public TrapType GetTrapType ()
     {
         return TrapType.Electicity;
     }
 
-    protected override bool PandaAttemptKill (PandaAI pandaAI, bool isPerfect)
+    public override void ActivateTrap ()
+    {
+        base.ActivateTrap();
+        electricity.SetActive(true);
+    }
+
+    public override void DeactivateTrap ()
+    {
+        base.DeactivateTrap();
+        electricity.SetActive(false);
+
+    }
+
+    override protected bool PandaAttemptKill (PandaAI pandaAI, bool isPerfect)
     {
         return pandaAI.AttemptDeathTrapKill(this, isPerfect);
     }
