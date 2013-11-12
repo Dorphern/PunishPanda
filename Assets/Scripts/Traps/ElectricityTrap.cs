df95e5d8a35849c04d1ff2695c7a7952f720b676
Kristoffer Dorph <kristofferdo@gmail.com>
2013-11-12 14:14:11 +0100

    Electricity trap groundwork

---
+++ Assets/Scripts/Traps/ElectricityTrap.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElectricityTrap : TrapBase {
+
+    override public TrapType GetTrapType ()
+    {
+        return TrapType.Electicity;
+    }
+
+    protected override bool PandaAttemptKill (PandaAI pandaAI, bool isPerfect)
+    {
+        return pandaAI.AttemptDeathTrapKill(this, isPerfect);
+    }
+
+}
