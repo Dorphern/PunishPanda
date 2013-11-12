eec53da2304e3ab355ab57d46bc6632776ffa879
RubenKLopez <RLopez5392@gmail.com>
2013-11-12 15:14:15 +0100

    completed merege

--- Assets/Scripts/Game/LevelData.cs
+++ Assets/Scripts/Game/LevelData.cs
@@ -7,18 +7,18 @@ public class LevelData
 {
     [SerializeField] public string LevelName = "";
     [SerializeField] public GameModes Mode;
-<<<<<<< HEAD
+
 	
 	public Texture2D funFactPicture;
 	public Texture2D objectiveScreen;
 	public string funFact;
 	public LevelScore score;
 	
-}
-=======
+
+
     [HideInInspector] [SerializeField] 
     public int HighScore;
     [HideInInspector] [SerializeField]
     public bool Unlocked; //1 == unlocked, 0 = locked
 }
->>>>>>> refs/heads/master
+
