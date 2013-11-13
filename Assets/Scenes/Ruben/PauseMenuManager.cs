34570c6f06c7af11a06938a06b199fad610b6d42
RubenKLopez <RLopez5392@gmail.com>
2013-11-13 12:31:51 +0100

    commit push

---
+++ Assets/Scenes/Ruben/PauseMenuManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuManager : MonoBehaviour {
+	
+	public GameObject PauseMenu; 
+	public GameObject HintScreen;
+	public GameObject LevelsScreen; 
+	
+	void Start()
+	{
+	    //HintScreen = GameObject.Find("Hint(Goal)Screen");
+	    //PauseMenu = GameObject.Find("PauseMENU");
+		//HintScreen.SetActiveRecursively(false);
+	}
+	
+	void OnClick()
+	{
+		//case for going In and out of HINT SCREEN
+		if(gameObject.name == "GoalButtonOffset")
+		{
+	    	HintScreen.SetActiveRecursively(true);
+	    	PauseMenu.SetActiveRecursively(false);
+		}
+		
+		if(gameObject.name == "HintBackButton")
+		{
+			HintScreen.SetActiveRecursively(false);
+	    	PauseMenu.SetActiveRecursively(true);
+		}
+		
+		//case for going In and out of LEVELS SCREEN
+		if(gameObject.name == "LevelsButtonOffset")
+		{
+			LevelsScreen.SetActiveRecursively(true);
+	    	PauseMenu.SetActiveRecursively(false);
+		}
+		if(gameObject.name == "LevelsBackButton")
+		{
+			LevelsScreen.SetActiveRecursively(false);
+	    	PauseMenu.SetActiveRecursively(true);
+		}
+	}
+	
+}
