18ed0811e9f9d5c83f4d6ed2800e8d5af4511381
Kristoffer Dorph <kristofferdo@gmail.com>
2013-11-13 14:54:31 +0100

    Finishing up the electricity trap

--- Assets/Scripts/Traps/ElectricityTrap.cs
+++ Assets/Scripts/Traps/ElectricityTrap.cs
@@ -7,6 +7,10 @@ public class ElectricityTrap : TrapBase {
     [SerializeField] GameObject electricityBox0;
     [SerializeField] GameObject electricityBox1;
 
+    [SerializeField] Texture[] electricTextures;
+    protected int electricIndex = 0;
+    protected int baseRotation = 45;
+
     override public TrapType GetTrapType ()
     {
         return TrapType.Electicity;
@@ -16,13 +20,27 @@ public class ElectricityTrap : TrapBase {
     {
         base.ActivateTrap();
         electricity.SetActive(true);
+        StartCoroutine(PlayTextureChange());
     }
 
     public override void DeactivateTrap ()
     {
         base.DeactivateTrap();
         electricity.SetActive(false);
+    }
 
+    IEnumerator PlayTextureChange ()
+    {
+        while (IsActive())
+        {
+            electricity.renderer.material.mainTexture = electricTextures[electricIndex];
+            electricIndex++;
+            if (electricIndex == electricTextures.Length) electricIndex = 0;
+            electricity.transform.eulerAngles = 
+                new Vector3(0, 0, Random.Range(0, 2) * 180 + baseRotation);
+            //electricIndex = Random.Range(0, electricTextures.Length);
+            yield return new WaitForSeconds(0.07f);
+        }
     }
 
     override protected bool PandaAttemptKill (PandaAI pandaAI, bool isPerfect)
