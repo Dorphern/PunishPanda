20ea6efd9e0d0c22eae712435871e911f77582d8
Dimitrios Katsaros <rth738@alumni.ku.dk>
2013-11-13 11:51:07 +0100

    Main menu and settings implemented

--- Assets/Scripts/Game/NGUI_Scripts/Settings.cs
+++ Assets/Scripts/Game/NGUI_Scripts/Settings.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class Settings : MonoBehaviour {
 
 	MenuManager menuMan;
+	public UISlider _musicSlider;
+	public UISlider _soundEFXSlider;
 	
 	void Start()
 	{
 		menuMan = GetComponent<MenuManager>();	
+
 	}
 	
 	public void OnCalibrateFingerClicked()
@@ -23,5 +26,27 @@ public class Settings : MonoBehaviour {
 	public void OnReturnClicked()
 	{
 		menuMan.SwitchToMenu(MenuTypes.MainMenu);
+	}
+	
+	public void OnLanguageClicked()
+	{
+		Debug.Log("Language Changed!");
+	}
+	
+	// crappy workaround, there is no delegate that returns the value ffs!
+	public void OnMusicSliderChanged()
+	{
+		if(_musicSlider!=null)
+		{
+			Debug.Log("music Slider val: " + _musicSlider.value);
+		}
+	}
+	
+	public void OnSoundEFXSliderChanged()
+	{
+		if(_musicSlider!=null)
+		{
+			Debug.Log("efx Slider val: " + _soundEFXSlider.value);
+		}
 	}
 }
