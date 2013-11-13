13802ddc09163d251da43b5c34013a435c2248e5
Dimitrios Katsaros <rth738@alumni.ku.dk>
2013-11-13 09:01:40 +0100

    Implemented the menu manager

---
+++ Assets/Scripts/Game/NGUI_Scripts/MenuType.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum MenuTypes
+{
+	None,
+	MainMenu,
+	Settings,
+	Achievements,
+	Unlocks
+}
+
+public class MenuType : MonoBehaviour {
+	
+	public MenuTypes type;
+	
+}
