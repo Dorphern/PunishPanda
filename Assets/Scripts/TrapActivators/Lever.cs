2bdfab917c3f6b7ee9c1f5497d8807267eb4d58e
Kristoffer Dorph <kristofferdo@gmail.com>
2013-11-21 14:13:10 +0100

    Implemented animation on trap activators

--- Assets/Scripts/TrapActivators/Lever.cs
+++ Assets/Scripts/TrapActivators/Lever.cs
@@ -2,18 +2,64 @@
 using System.Collections;
 
 public class Lever : TrapActivator {
-	
-	public TrapBase trap;
+
+    [SerializeField] bool isActive = false;
+    private string animationName = "lever";
+
 	private Collidable colliderType;
+
+    void Start ()
+    {
+        if (isActive)
+        {
+            ActivateTraps();
+        }
+    }
+
+    protected override void ActivateTraps ()
+    {
+        base.ActivateTraps();
+        StartCoroutine(PlayActivateAnimation());
+    }
+
+    protected override void DeactivateTraps ()
+    {
+        base.DeactivateTraps();
+        PlayDeactiveAnimation();
+    }
 	
 	void OnTriggerEnter(Collider collider)
 	{
+        Debug.Log("enter!");
 		colliderType = collider.GetComponent<Collidable>();
 		if(colliderType == null) return;
 		
 		if(colliderType.type == CollidableTypes.Panda)
 		{
-            TriggerTraps();
+            if (isActive)
+            {
+                DeactivateTraps();
+            }
+            else
+            {
+                ActivateTraps();
+            }
+
+            isActive = !isActive;
 		}
-	}
+    }
+
+    IEnumerator PlayActivateAnimation ()
+    {
+        Debug.Log("activate anim");
+        animation.Play(animationName);
+        yield return new WaitForSeconds(0.2f);
+        animation[animationName].speed = 0;
+    }
+
+    void PlayDeactiveAnimation ()
+    {
+        Debug.Log("deactivate anim");
+        animation[animationName].speed = 1;
+    }
 }
