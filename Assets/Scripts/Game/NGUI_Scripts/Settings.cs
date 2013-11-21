c9173c4fd2ff5fe5de7f5cc45abdc98b2a635c61
RubenKLopez <RLopez5392@gmail.com>
2013-11-21 12:15:38 +0100

    saving changes

--- Assets/Scripts/Game/NGUI_Scripts/Settings.cs
+++ Assets/Scripts/Game/NGUI_Scripts/Settings.cs
@@ -8,23 +8,36 @@ public class Settings : MonoBehaviour {
 	public UISlider _soundEFXSlider;
 	public UILabel languageLabel;
 	
+	public GameObject SFX_Lever;
+	public GameObject Music_Lever;
+	private Animation SFX_animation;
+	private Animation Music_animation;
+	private int MusicAnimationFlag;
+	private int SFXAnimationFlag;
+	
 	void Awake()
 	{
-		menuMan = GetComponent<MenuManager>();	
+		menuMan = GetComponent<MenuManager>();
+		SFX_animation = SFX_Lever.GetComponent<Animation>();
+		Music_animation = Music_Lever.GetComponent<Animation>();
+		
 		
 		bool music = InstanceFinder.StatsManager.musicEnabled;
 		bool sound = InstanceFinder.StatsManager.soundEffectsEnabled;
 		string lang = InstanceFinder.StatsManager.language;
 		
+
 		if(music)
 			_musicSlider.value = 1;
 		else
 			_musicSlider.value = 0;
+
 		
 		if(sound)
 			_soundEFXSlider.value = 1;
 		else
 			_soundEFXSlider.value = 0;
+
 		
 		if(languageLabel!=null)
 		{
@@ -44,7 +57,13 @@ public class Settings : MonoBehaviour {
 			}
 		}
 		// language button initialization goes here
+		Debug.Log ("settings script awaken");
 		
+		//initialize Switches
+		MusicAnimationFlag = 0;
+		SFXAnimationFlag = 0;
+		OnSoundEFXSliderChanged();
+		OnMusicSliderChanged();
 	}
 	
 	public void OnCalibrateFingerClicked()
@@ -62,31 +81,39 @@ public class Settings : MonoBehaviour {
 		menuMan.SwitchToMenu(MenuTypes.MainMenu);
 	}
 	
-	// placeholder code for the placeholder button
-	public void OnLanguageClicked()
+	public void OnEnglishClicked()
 	{
-		if(languageLabel!=null)
+		if(InstanceFinder.Localization.currentLanguage!="English")
 		{
 			for(int i=0;i<InstanceFinder.Localization.languages.Length; i++)
 			{
-				if(InstanceFinder.Localization.languages[i].name!=languageLabel.text)
+				if(InstanceFinder.Localization.languages[i].name=="English")
 				{
-					if(InstanceFinder.Localization.languages.Length == i-1)
-					{
-						InstanceFinder.Localization.currentLanguage = InstanceFinder.Localization.languages[0].name;
-						languageLabel.text = InstanceFinder.Localization.languages[0].name;
-						InstanceFinder.StatsManager.language = InstanceFinder.Localization.languages[0].name;
-						InstanceFinder.StatsManager.Save();
-						break;
-					}
-					else
-					{
-						InstanceFinder.Localization.currentLanguage = InstanceFinder.Localization.languages[i].name;
-						languageLabel.text = InstanceFinder.Localization.languages[i].name;
-						InstanceFinder.StatsManager.language = InstanceFinder.Localization.languages[i].name;
-						InstanceFinder.StatsManager.Save();
-						break;
-					}
+					InstanceFinder.Localization.currentLanguage = InstanceFinder.Localization.languages[i].name;
+					InstanceFinder.StatsManager.language = InstanceFinder.Localization.languages[i].name;
+					InstanceFinder.StatsManager.Save();
+					break;	
+				}
+			}
+		}
+	}
+	
+	public void OnDanishClicked()
+	{
+		
+		if(InstanceFinder.Localization.currentLanguage!="Danish")
+		{
+			Debug.Log(InstanceFinder.Localization.languages.Length);
+			for(int i=0;i<InstanceFinder.Localization.languages.Length; i++)
+			{
+				Debug.Log(InstanceFinder.Localization.languages[i].name);
+				if(InstanceFinder.Localization.languages[i].name=="Danish")
+				{
+					
+					InstanceFinder.Localization.currentLanguage = InstanceFinder.Localization.languages[i].name;
+					InstanceFinder.StatsManager.language = InstanceFinder.Localization.languages[i].name;
+					InstanceFinder.StatsManager.Save();
+					break;	
 				}
 			}
 		}
@@ -98,13 +125,29 @@ public class Settings : MonoBehaviour {
 	{
 		if(_musicSlider!=null)
 		{
+			//hack for dealing with extra animation (on start)
+			if(MusicAnimationFlag == 1)
+			{	
+				MusicAnimationFlag++;
+				return;
+			}
+			else
+				MusicAnimationFlag++;
+			
 			if(_musicSlider.value==0)
 			{
+				Debug.Log ("Music OFF");
+				Music_animation.Play ("leverAnimation2");//goleft
+	
 				InstanceFinder.StatsManager.musicEnabled = false;
 				InstanceFinder.StatsManager.Save();
 			}
 			else
 			{
+				Debug.Log ("Music ON");
+
+				Music_animation.Play ("leverAnimation1");//goright
+
 				InstanceFinder.StatsManager.musicEnabled = true;
 				InstanceFinder.StatsManager.Save();
 			}
@@ -113,17 +156,31 @@ public class Settings : MonoBehaviour {
 	
 	public void OnSoundEFXSliderChanged()
 	{
-		if(_musicSlider!=null)
+		if(_soundEFXSlider!=null)
 		{
+			//hack for dealing with extra animation (on start)
+			if(SFXAnimationFlag == 1)
+			{	
+				SFXAnimationFlag++;
+				return;
+			}
+			else
+				SFXAnimationFlag++;
+			
 			if(_soundEFXSlider.value==0)
 			{
 				Debug.Log ("Sound OFF");
+
+				SFX_animation.Play ("leverAnimation2");//goleft
+
 				InstanceFinder.StatsManager.soundEffectsEnabled = false;
 				InstanceFinder.StatsManager.Save();
 			}
 			else
 			{
 				Debug.Log ("Sound ON");
+				SFX_animation.Play ("leverAnimation1");//goright		
+				
 				InstanceFinder.StatsManager.soundEffectsEnabled = true;
 				InstanceFinder.StatsManager.Save();
 			}
