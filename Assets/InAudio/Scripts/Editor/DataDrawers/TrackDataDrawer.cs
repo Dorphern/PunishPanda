868b5221eb8914cf4e84a9422d8e5e10609184c7
Peter <peter.oe@gmail.com>
2013-11-21 13:23:48 +0100

    Added new audio

---
+++ Assets/InAudio/Scripts/Editor/DataDrawers/TrackDataDrawer.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace InAudio.HDREditorGUI
+{
+public static class TrackDataDrawer
+{
+    public static void Draw(AudioNode node)
+    {
+        UndoHandler.CheckUndo(new UnityEngine.Object[] { node, node.NodeData });
+        node.Name = EditorGUILayout.TextField("Name", node.Name);
+        NodeTypeDataDrawer.Draw(node);
+        UndoHandler.CheckGUIChange();
+    }
+}
+
+}
---
+++ Assets/InAudio/Scripts/Pools/RuntimeInfoPool.cs
@@ -0,0 +1,15 @@
+using InAudio;
+
+public class RuntimeInfoPool : ObjectPool<RuntimeInfo>{
+    public new RuntimeInfo GetObject()
+    {
+        if (freeObjects.Count == 0)
+        {
+            ReserveExtra(allocateSize);
+        }
+
+        var go = freeObjects[freeObjects.Count - 1];
+        freeObjects.RemoveAt(freeObjects.Count - 1);
+        return go;
+    }
+}
