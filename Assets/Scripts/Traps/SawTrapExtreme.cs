52e7648e99bb671cb79df41e7a4b03d5830f95be
Kristoffer Dorph <kristofferdo@gmail.com>
2013-11-22 16:05:39 +0100

    Adding extreme saw trap, and fixing normal saw

---
+++ Assets/Scripts/Traps/SawTrapExtreme.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SawTrapExtreme : TrapBase {
+
+    [SerializeField] GameObject dirtySaw;
+    [SerializeField] GameObject cleanSaw;
+
+    # region Public Methods
+
+    override public TrapType GetTrapType ()
+    {
+        return TrapType.RoundSaw;
+    }
+
+    # endregion
+
+
+    # region Private Methods
+
+    public override void SetClean ()
+    {
+        dirty = false;
+        dirtySaw.SetActive(dirty);
+        cleanSaw.SetActive(!dirty);
+    }
+
+    public override void SetDirty ()
+    {
+        dirty = true;
+        dirtySaw.SetActive(dirty);
+        cleanSaw.SetActive(!dirty);
+    }
+
+    override protected bool PandaAttemptKill (PandaAI pandaAI, bool isPerfect)
+    {
+        return pandaAI.AttemptDeathTrapKill(this, isPerfect);
+    }
+
+    # endregion
+
+}
