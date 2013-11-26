29a520506ea7e2e59c04a39a103f0b80cf3ce2b7
Dimitrios Katsaros <rth738@alumni.ku.dk>
2013-11-26 15:06:53 +0100

    Added changes for win screen

--- Assets/Scripts/Game/NGUI_Scripts/WinScreen.cs
+++ Assets/Scripts/Game/NGUI_Scripts/WinScreen.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SocialPlatforms.Impl;
+using PunishPanda;
 
 public class WinScreen : MonoBehaviour {
 	
@@ -8,13 +11,19 @@ public class WinScreen : MonoBehaviour {
 	public UISprite oneStarTexture;
 	public UISprite twoStarTexture;
 	public UISprite threeStarTexture;
-	public UILabel   FunFactsLabel;
-	public UILabel   scoreLabel;
-	public UILabel   highScoreLabel;
+	public UILabel  FunFactsLabel;
+	public UILabel  scoreLabel;
+	public UILabel  ScoreTypeLabel;
     public UILabel  newHighScoreLabel;
+	public UILabel  TotalScoreLabel;
+	
+	public AnimationCurve ScoreCurve;
+	public float scoreCurveDuration;
+	
+
 	
 	public void OnLevelsButtonClicked()
-	{
+	{ 
 		InstanceFinder.LevelManager.LoadLevelsMenu();
 	}
 	
@@ -70,7 +79,7 @@ public class WinScreen : MonoBehaviour {
             levelData.HighScore = score;
         }
 
-        scoreLabel.text = score.ToString();
+        //scoreLabel.text = score.ToString();
         
         /*// we also need to deactivate controls
         //this is where calcualtions for score, stars and adding to lifetime score happens
@@ -99,20 +108,21 @@ public class WinScreen : MonoBehaviour {
         // star calculation
 
         int stars = level.Stars();
-        if(stars==1)
+//        if(stars==1)
+//        {
+//            oneStarTexture.gameObject.SetActive(true);
+//        }
+//        else if(stars==2)
+//        {
+//            oneStarTexture.gameObject.SetActive(true);
+//            twoStarTexture.gameObject.SetActive(true);
+//        }
+		
+		StartCoroutine(PlayAnimations(stars));
+		StartCoroutine(PlayScore());
+		
+        if(stars==3)
         {
-            oneStarTexture.gameObject.SetActive(true);
-        }
-        else if(stars==2)
-        {
-            oneStarTexture.gameObject.SetActive(true);
-            twoStarTexture.gameObject.SetActive(true);
-        }
-        else if(stars==3)
-        {
-            oneStarTexture.gameObject.SetActive(true);
-            twoStarTexture.gameObject.SetActive(true);
-            threeStarTexture.gameObject.SetActive(true);
 			if(InstanceFinder.AchievementManager != null)
 			{
 				// if the achievement has been completed
@@ -126,7 +136,144 @@ public class WinScreen : MonoBehaviour {
         }
 
     }
-
+	
+	IEnumerator PlayAnimations(int stars)
+	{
+		if(oneStarTexture!=null && stars>=1)
+			oneStarTexture.gameObject.SetActive(true);
+		
+		yield return new WaitForSeconds(0.4f);
+		
+		if(twoStarTexture!=null && stars>=2)
+			twoStarTexture.gameObject.SetActive(true);
+		
+		yield return new WaitForSeconds(0.4f);
+		
+		if(threeStarTexture!=null && stars==3)
+			threeStarTexture.gameObject.SetActive(true);
+		
+	}
+	
+	IEnumerator PlayScore()
+	{
+		List<ComboKill> kills = InstanceFinder.ComboSystem.LevelDeaths.ComboKills;
+		PointSystem pointSystem = InstanceFinder.PointSystem;
+		int timeScore = InstanceFinder.GameManager.ActiveLevel.GetTimeScore();
+		int total = 0, intermediateTotal = 0;
+		int normalKills = 0, perfectKills = 0;
+		float startTime, timeElapsed;
+		
+	
+		// for every panda present the kill
+		for(int i=0; i<kills.Count; i++)
+		{
+			
+			ComboKill ck = kills[i];
+		
+			normalKills += ck.NormalKills;
+			perfectKills += ck.PerfectKills;			
+		}
+		// normal kills
+		ScoreTypeLabel.text = "Normal Kill";
+		intermediateTotal = pointSystem.PerKill;
+		for(int i=0; i< normalKills; i++)
+		{
+			//score increment loop
+		
+			startTime = Time.realtimeSinceStartup;
+			timeElapsed = Time.realtimeSinceStartup - startTime;
+			
+			while(timeElapsed < scoreCurveDuration)
+			{
+				Debug.Log(ScoreCurve.Evaluate(timeElapsed/scoreCurveDuration));
+				scoreLabel.text = (( ScoreCurve.Evaluate(timeElapsed/scoreCurveDuration)*intermediateTotal)).ToString("N0");
+				TotalScoreLabel.text = (total + ( ScoreCurve.Evaluate(timeElapsed/scoreCurveDuration)*intermediateTotal)).ToString("N0");
+				yield return null;
+				timeElapsed = Time.realtimeSinceStartup - startTime;
+			}
+			total += intermediateTotal;
+			yield return new WaitForSeconds(1f);
+			
+		}
+		
+		// perfect kills
+		ScoreTypeLabel.text = "Perfect Kill";
+		intermediateTotal = pointSystem.PerfectKill;
+		for(int i=0; i< perfectKills; i++)
+		{
+			//score increment loop
+		
+			startTime = Time.realtimeSinceStartup;
+			timeElapsed = Time.realtimeSinceStartup - startTime;
+			
+			while(timeElapsed < scoreCurveDuration)
+			{
+				Debug.Log(ScoreCurve.Evaluate(timeElapsed/scoreCurveDuration));
+				scoreLabel.text = (( ScoreCurve.Evaluate(timeElapsed/scoreCurveDuration)*intermediateTotal)).ToString("N0");
+				TotalScoreLabel.text = (total + ( ScoreCurve.Evaluate(timeElapsed/scoreCurveDuration)*intermediateTotal)).ToString("N0");
+				yield return null;
+				timeElapsed = Time.realtimeSinceStartup - startTime;
+			}
+			total += intermediateTotal;
+			yield return new WaitForSeconds(1f);
+			
+		}
+		
+		// combo kills
+		for(int i=0; i<kills.Count; i++)
+		{	
+			ComboKill ck = kills[i];
+			
+			if(ck.ComboCount > 1)
+			{
+				ScoreTypeLabel.text = ck.ComboCount + "x Combo";
+				intermediateTotal = pointSystem.Combo * ck.ComboCount;
+				
+				
+				startTime = Time.realtimeSinceStartup;
+				timeElapsed = Time.realtimeSinceStartup - startTime;
+				
+				while(timeElapsed < scoreCurveDuration)
+				{
+					Debug.Log(ScoreCurve.Evaluate(timeElapsed/scoreCurveDuration));
+					scoreLabel.text = (( ScoreCurve.Evaluate(timeElapsed/scoreCurveDuration)*intermediateTotal)).ToString("N0");
+					TotalScoreLabel.text = (total + ( ScoreCurve.Evaluate(timeElapsed/scoreCurveDuration)*intermediateTotal)).ToString("N0");
+					yield return null;
+					timeElapsed = Time.realtimeSinceStartup - startTime;
+				}
+				total += intermediateTotal;
+				yield return new WaitForSeconds(1f);
+			}
+		}
+		
+		// time score calc
+		ScoreTypeLabel.text = "Time";
+		
+		
+		intermediateTotal = timeScore;
+		
+		startTime = Time.realtimeSinceStartup;
+		timeElapsed = Time.realtimeSinceStartup - startTime;
+		
+		
+		if(timeScore!=0)
+		{
+			while(timeElapsed < scoreCurveDuration)
+			{
+				scoreLabel.text = (( ScoreCurve.Evaluate(timeElapsed/scoreCurveDuration)*timeScore)).ToString("N0");
+				TotalScoreLabel.text = (total + ( ScoreCurve.Evaluate(timeElapsed/scoreCurveDuration)*intermediateTotal)).ToString("N0");
+				yield return null;
+				timeElapsed = Time.realtimeSinceStartup - startTime;
+			}
+		}
+		else
+			scoreLabel.text = (0).ToString();
+		total += intermediateTotal;
+		
+		yield return new WaitForSeconds(1f);
+			
+	}
+	
     private static void UnLockLevels()
     {
         var levelManager = InstanceFinder.LevelManager;
