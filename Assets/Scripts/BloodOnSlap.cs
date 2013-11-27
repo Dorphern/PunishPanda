0de226b7fc053aaa05ab520727afa7941f2eda60
katja <katjalindskrone@gmail.com>
2013-11-27 11:00:41 +0100

    merge

--- Assets/Scripts/BloodOnSlap.cs
+++ Assets/Scripts/BloodOnSlap.cs
@@ -10,9 +10,12 @@ public class BloodOnSlap : MonoBehaviour {
 	 *  Function is called on a Slap (in PandaAI).
 	*********************************************************/
 		
-	private PandaStateManager pandaStateManager;
+	public GameObject PandaMouthFront; //use for start position of emmision
+	public GameObject PandaMouthBack; //use for turn-slap
 	public Version particleVersion;
 	private string objectName;
+	private float timeStamp1;
+	private float timeStamp2;
 	
 	// 3D vector controlling the direction of the blood particle object
 	private Vector3 projectionDirection = Vector3.right;
@@ -21,8 +24,7 @@ public class BloodOnSlap : MonoBehaviour {
 	[SerializeField] List<AudioEvent> slapAudioEvents = new List<AudioEvent>();
 	
 	void Start ()
-    {
-        pandaStateManager = GetComponent<PandaStateManager>();			
+    {	
 		if(particleVersion == Version.v1)
 		{
 			objectName = "particle_slap_v1";	
@@ -35,8 +37,11 @@ public class BloodOnSlap : MonoBehaviour {
 	
 	
 	//Method for emmiting blood in the same direction(and angle) as slap.
-	public void EmmitSlapBloodWithAngle(Vector2 slapDirection)
+	//NOTE: we use this when panda is slapped in the BACK.
+	public void EmmitSlapBlood(Vector2 slapDirection)
 	{
+		float diff;
+		
 		for(int i = 0; i < slapAudioEvents.Count; ++i)
 		{
 			
@@ -48,9 +53,54 @@ public class BloodOnSlap : MonoBehaviour {
 		projectionDirection.y = slapDirection.y;
 		projectionDirection.z = Mathf.Abs(slapDirection.x);
 		
-		//Object instantiates facing the Z-axis direction	
-		Instantiate(Resources.Load(objectName),transform.position, Quaternion.LookRotation(projectionDirection));
 		
+		timeStamp1 = Time.realtimeSinceStartup;
+		//Debug.Log (timeStamp1);
+		
+		diff = timeStamp1 - timeStamp2;
+		diff = Mathf.Abs (diff);
+		//Debug.Log ("diff:"+diff);
+		
+		if(diff > 0.1)
+		{
+			//Object instantiates facing the Z-axis direction
+			Instantiate(Resources.Load(objectName),PandaMouthFront.transform.position, Quaternion.LookRotation(projectionDirection));
+		}
+
+
+	} 
+	
+	//NOTE: use this when panda is slapped from its FRONT (and does a turn)
+	public void EmmitSlapBloodOnTurn(Vector2 slapDirection)
+	{
+		float diff;
+		
+		for(int i = 0; i < slapAudioEvents.Count; ++i)
+		{
+			
+			HDRSystem.PostEvent(gameObject, slapAudioEvents[i]);
+		}
+		
+		// set the angle of the splat to be the same in both XY and XZ planes
+		projectionDirection.x = slapDirection.x;
+		projectionDirection.y = slapDirection.y;
+		projectionDirection.z = Mathf.Abs(slapDirection.x);
+		
+		timeStamp2 = Time.realtimeSinceStartup;
+		//Debug.Log (timeStamp2);
+		
+		diff = timeStamp1 - timeStamp2;
+		diff = Mathf.Abs (diff);
+		//Debug.Log ("diff:"+diff);
+		//diff registered at 0.018 sec away...
+		
+		
+		if(diff > 0.1)
+		{
+			//Object instantiates facing the Z-axis direction	
+			Instantiate(Resources.Load(objectName),PandaMouthBack.transform.position, Quaternion.LookRotation(projectionDirection));
+		}
+
 
 	} 
 }
