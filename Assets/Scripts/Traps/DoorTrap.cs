56b4ab26cbfa934ddd2dc714f64713404a959b5e
unknown <paj@e3600-03.fag.filmskolen.dk>
2013-11-28 11:53:18 +0100

    made the trap doors work on init, howevrer look bad, Cristi needs to fix

--- Assets/Scripts/Traps/DoorTrap.cs
+++ Assets/Scripts/Traps/DoorTrap.cs
@@ -6,7 +6,7 @@ public class DoorTrap : TrapBase {
 	public string animationName;
 	private AnimationState trapClip;
 	
-	void Start()
+	void Awake()
 	{
 		trapClip = this.animation[animationName];	
 	}
