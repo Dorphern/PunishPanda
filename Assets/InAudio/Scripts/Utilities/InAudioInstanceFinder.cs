5c51932c9fa5d911e475ee856401611917ee8d86
Peter <peter.oe@gmail.com>
2013-11-29 14:21:25 +0100

    -Added panda halves -Readded unlock screen -Added blood effect

--- Assets/InAudio/Scripts/Utilities/InAudioInstanceFinder.cs
+++ Assets/InAudio/Scripts/Utilities/InAudioInstanceFinder.cs
@@ -4,6 +4,7 @@ using UnityEditor;
 #endif
 using UnityEngine;
 
+[AddComponentMenu(FolderSettings.ComponentPathInternalManager + "Instance Finder")]
 public class InAudioInstanceFinder : MonoBehaviour
 {
     private static InAudioInstanceFinder instance;
