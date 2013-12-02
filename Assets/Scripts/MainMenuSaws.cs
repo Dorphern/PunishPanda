acb901b568011d282e010748338b4b8289b2656f
RubenKLopez <RLopez5392@gmail.com>
2013-12-02 14:15:52 +0100

    beta main menu saws + settings levers

---
+++ Assets/Scripts/MainMenuSaws.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuSaws : MonoBehaviour {
+	
+	public GameObject sawObject;
+	SawTrap sawtrap;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+		sawtrap = sawObject.GetComponent<SawTrap>();
+		
+	}
+	
+	public void OnPress(bool isDown)
+	{
+	    if(isDown)
+	    {
+		   Debug.Log ("PRESSING");
+	       sawtrap.ActivateTrap();
+	    }
+	 
+	    if(!isDown)
+	    {
+			 Debug.Log ("DEACTIVATE");
+	       sawtrap.DeactivateTrap();
+	    }
+	} 
+
+	
+}
