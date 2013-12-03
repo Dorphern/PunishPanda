adb34966a4129d7ec9123133c682c91e0ecc2efa
Kristoffer Dorph <kristofferdo@gmail.com>
2013-12-03 17:23:40 +0100

    removed log and added metas

--- Assets/Scripts/PandaAI.cs
+++ Assets/Scripts/PandaAI.cs
@@ -80,7 +80,6 @@ public class PandaAI : MonoBehaviour {
             if (pandaStateManager.GetState() == PandaState.Idle)
             {
                 animations.ChangePandaState(PandaState.Idle);
-                Debug.Log("halla");
             }
 			pandaStateManager.ChangeState(PandaState.Idle);
 			BloodSplatter.Instance.ProjectHit(transform.position, new Vector2(0f, - 0.2f));
