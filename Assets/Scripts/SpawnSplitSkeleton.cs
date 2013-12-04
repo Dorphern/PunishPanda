d6b847433b7538c52205397adc0776c987ca8c78
cristi16 <patrascu.cristinel@gmail.com>
2013-12-04 22:07:09 +0100

    removed panda disolving animation

--- Assets/Scripts/SpawnSplitSkeleton.cs
+++ Assets/Scripts/SpawnSplitSkeleton.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class SpawnSplitSkeleton : MonoBehaviour {
 	[SerializeField] protected GameObject splitSkeletonPanda;
 	public float timeBeforeSpawn = 2f;
-	public float teethDisableTime = 1.6f;
 	public float ashSilhouetteTime = 0.8f;
 	public Renderer teethOutline;
 	public Renderer ashSilhouette;
@@ -17,10 +16,8 @@ public class SpawnSplitSkeleton : MonoBehaviour {
 	{
 		yield return new WaitForSeconds(ashSilhouetteTime);
 		ashSilhouette.enabled = true;
-		yield return new WaitForSeconds(teethDisableTime - ashSilhouetteTime);
 		teethOutline.enabled = false;
-		yield return new WaitForSeconds(timeBeforeSpawn - teethDisableTime);
-		//ashSilhouette.enabled = false;
+		yield return new WaitForSeconds(timeBeforeSpawn - ashSilhouetteTime);
 		Instantiate(splitSkeletonPanda, transform.position + new Vector3(0, 0f, 0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)));
 		Destroy(this.gameObject);
 	}
