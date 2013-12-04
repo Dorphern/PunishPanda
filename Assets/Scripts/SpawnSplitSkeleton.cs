2856289e53120f2589b05bcd24b418bbe19b2ea8
cristi16 <patrascu.cristinel@gmail.com>
2013-12-04 20:38:34 +0100

    added silhouette to electrocution animation

--- Assets/Scripts/SpawnSplitSkeleton.cs
+++ Assets/Scripts/SpawnSplitSkeleton.cs
@@ -5,7 +5,9 @@ public class SpawnSplitSkeleton : MonoBehaviour {
 	[SerializeField] protected GameObject splitSkeletonPanda;
 	public float timeBeforeSpawn = 2f;
 	public float teethDisableTime = 1.6f;
+	public float ashSilhouetteTime = 0.8f;
 	public Renderer teethOutline;
+	public Renderer ashSilhouette;
 	// Use this for initialization
 	void Start () {
 		StartCoroutine(SpawnSkeleton());
@@ -13,9 +15,12 @@ public class SpawnSplitSkeleton : MonoBehaviour {
 	
 	IEnumerator SpawnSkeleton()
 	{
-		yield return new WaitForSeconds(teethDisableTime);
+		yield return new WaitForSeconds(ashSilhouetteTime);
+		ashSilhouette.enabled = true;
+		yield return new WaitForSeconds(teethDisableTime - ashSilhouetteTime);
 		teethOutline.enabled = false;
 		yield return new WaitForSeconds(timeBeforeSpawn - teethDisableTime);
+		//ashSilhouette.enabled = false;
 		Instantiate(splitSkeletonPanda, transform.position + new Vector3(0, 0f, 0f), Quaternion.Euler(new Vector3(0f, 180f, 0f)));
 		Destroy(this.gameObject);
 	}
